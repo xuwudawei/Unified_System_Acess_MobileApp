156287e476127140ad42f617bb312cc39ebbf861
xuwudawei <xuwudawei@outlook.com>
2021-12-26 02:20:04 +0000

    Home page design done

--- lib/screens/home/homePage.dart
+++ lib/screens/home/homePage.dart
@@ -1,4 +1,10 @@
+import 'dart:ui';
+
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:usa_app/providers/productListProvider.dart';
+import 'package:usa_app/screens/home/homePageProductTile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,6 +16,135 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final productListProv = Provider.of<ProductListProvider>(context);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              child: DefaultTextStyle(
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width / 18.5,
+                  fontFamily: 'Agne',
+                  fontWeight: FontWeight.bold,
+                ),
+                child: AnimatedTextKit(
+                  animatedTexts: [
+                    TypewriterAnimatedText('Hi, David'),
+                    TypewriterAnimatedText('Welcome to Unified System Access'),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Expanded(
+              child: Stack(
+                children: [
+                  Column(
+                    children: [
+                      Container(
+                        width: 200,
+                        height: 200,
+                        margin: EdgeInsets.only(
+                            left: MediaQuery.of(context).size.width / 2.4),
+                        decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                Colors.pink.shade700,
+                                Colors.orange.shade500
+                              ],
+                              begin: Alignment.topRight,
+                              end: Alignment.bottomLeft,
+                            ),
+                            borderRadius: BorderRadius.circular(100)),
+                      ),
+                      Container(
+                        width: 100,
+                        height: 100,
+                        margin: EdgeInsets.only(right: 270),
+                        decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                Colors.pink.shade700,
+                                Colors.orange.shade500
+                              ],
+                              begin: Alignment.topRight,
+                              end: Alignment.bottomLeft,
+                            ),
+                            borderRadius: BorderRadius.circular(100)),
+                      ),
+                      Container(
+                        width: 200,
+                        height: 200,
+                        margin: EdgeInsets.only(
+                            left: MediaQuery.of(context).size.width / 2.4),
+                        decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                Colors.pink.shade700,
+                                Colors.orange.shade500
+                              ],
+                              begin: Alignment.topRight,
+                              end: Alignment.bottomLeft,
+                            ),
+                            borderRadius: BorderRadius.circular(100)),
+                      ),
+                      Container(
+                        width: 100,
+                        height: 100,
+                        margin: EdgeInsets.only(right: 270),
+                        decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                Colors.pink.shade700,
+                                Colors.orange.shade500
+                              ],
+                              begin: Alignment.topRight,
+                              end: Alignment.bottomLeft,
+                            ),
+                            borderRadius: BorderRadius.circular(100)),
+                      ),
+                    ],
+                  ),
+                  ListView.builder(
+                    itemCount: productListProv.getAllProducts.length,
+                    shrinkWrap: true,
+                    physics: const AlwaysScrollableScrollPhysics(),
+                    itemBuilder: (context, index) {
+                      return Container(
+                        child: ProductTile(
+                          productName:
+                              productListProv.getAllProducts[index].productName,
+                          productImage: productListProv
+                              .getAllProducts[index].productImage,
+                          date: productListProv.getAllProducts[index].date,
+                          productDescription: productListProv
+                              .getAllProducts[index].productDescription,
+                          acccountStatus: productListProv
+                              .getAllProducts[index].accountStatus,
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
