72ec1b07aab04d5e51d1281e0be7bbe3a770ff5c
xuwudawei <xuwudawei@outlook.com>
2021-12-26 02:18:26 +0000

    product model done

---
+++ lib/models/product_model.dart
@@ -0,0 +1,15 @@
+class ProductModel {
+  String? productName;
+  String? productDescription;
+  String? productImage;
+  String? date;
+  String? accountStatus;
+
+  ProductModel({
+    this.productName,
+    this.productDescription,
+    this.productImage,
+    this.date,
+    this.accountStatus,
+  });
+}
