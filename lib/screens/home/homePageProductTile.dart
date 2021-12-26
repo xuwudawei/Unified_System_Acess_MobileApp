aca1fa585c305c9f91ee0e1e1494c88404a31d1b
xuwudawei <xuwudawei@outlook.com>
2021-12-26 16:43:50 +0000

    added center to product tile

--- lib/screens/home/homePageProductTile.dart
+++ lib/screens/home/homePageProductTile.dart
@@ -42,140 +42,142 @@ class ProductTile extends StatelessWidget {
       }
     }
 
-    return Container(
-      padding: EdgeInsets.only(bottom: 20),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(20),
-        child: Container(
-          child: Stack(
-            children: [
-              BackdropFilter(
-                filter: ImageFilter.blur(
-                  sigmaX: 7,
-                  sigmaY: 7,
+    return Center(
+      child: Container(
+        padding: EdgeInsets.only(bottom: 20),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Container(
+            child: Stack(
+              children: [
+                BackdropFilter(
+                  filter: ImageFilter.blur(
+                    sigmaX: 7,
+                    sigmaY: 7,
+                  ),
+                  // child: Container(
+                  //   height: MediaQuery.of(context).size.height * 0.3,
+                  //   width: MediaQuery.of(context).size.width / 2.5,
+                  // ),
                 ),
-                // child: Container(
-                //   height: MediaQuery.of(context).size.height * 0.3,
-                //   width: MediaQuery.of(context).size.width / 2.5,
-                // ),
-              ),
-              Container(
-                height: MediaQuery.of(context).size.height * 0.25,
-                width: MediaQuery.of(context).size.width / 1.07,
-                decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color: _detectAccountBackgroundColor()!,
-                      )
-                    ],
-                    border: Border.all(
-                        color: Colors.white.withOpacity(0.2), width: 1.0),
-                    gradient: LinearGradient(
-                      colors: [
-                        Colors.white.withOpacity(0.5),
-                        Colors.white.withOpacity(0.2)
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.25,
+                  width: MediaQuery.of(context).size.width / 1.07,
+                  decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: _detectAccountBackgroundColor()!,
+                        )
                       ],
-                      stops: [0.0, 1.0],
-                    ),
-                    borderRadius: BorderRadius.circular(20)),
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Row(
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(
-                            height: 10,
-                          ),
-                          SizedBox(
-                            width: MediaQuery.of(context).size.width / 1.8,
-                            child: Text(
-                              productName!,
-                              style: TextStyle(
-                                  color: Colors.white.withOpacity(0.6),
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 22),
-                            ),
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Text(
-                            productDescription!,
-                            style: TextStyle(
-                                fontSize: 23,
-                                color: Colors.white.withOpacity(0.4)),
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          SizedBox(
-                            width: MediaQuery.of(context).size.width / 1.8,
-                            child: Row(
-                              children: [
-                                Text(
-                                  "6372",
-                                  style: TextStyle(
-                                      color: Colors.white.withOpacity(0.5),
-                                      fontSize: 12),
-                                ),
-                                SizedBox(
-                                  width: 100,
-                                ),
-                                Text(
-                                  "Date \nCreated",
-                                  style: TextStyle(
-                                      fontSize: 10,
-                                      color: Colors.white.withOpacity(0.5),
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                Text(
-                                  "  " + date!,
-                                  style: TextStyle(
-                                      fontSize: 14,
-                                      color: Colors.white.withOpacity(0.5)),
-                                ),
-                              ],
-                            ),
-                          ),
-                          SizedBox(
-                            width: 275,
-                            child: Text(
-                              "Status:",
-                              style: TextStyle(
-                                  fontSize: 20,
-                                  color: Colors.white.withOpacity(0.6),
-                                  fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 8.0),
-                            child: Row(
-                              children: [
-                                Icon(
-                                  Icons.lock_outline,
-                                  color: _detectAccountColor()!,
-                                ),
-                                Text(" - " + acccountStatus!,
-                                    style: TextStyle(
-                                        fontSize: 20,
-                                        color: _detectAccountColor()!,
-                                        fontWeight: FontWeight.bold)),
-                              ],
-                            ),
-                          ),
+                      border: Border.all(
+                          color: Colors.white.withOpacity(0.2), width: 1.0),
+                      gradient: LinearGradient(
+                        colors: [
+                          Colors.white.withOpacity(0.5),
+                          Colors.white.withOpacity(0.2)
                         ],
+                        stops: [0.0, 1.0],
                       ),
-                      Image.asset(
-                        productImage!,
-                        height: MediaQuery.of(context).size.width / 10,
-                      ),
-                    ],
+                      borderRadius: BorderRadius.circular(20)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Row(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              height: 10,
+                            ),
+                            SizedBox(
+                              width: MediaQuery.of(context).size.width / 1.8,
+                              child: Text(
+                                productName!,
+                                style: TextStyle(
+                                    color: Colors.white.withOpacity(0.6),
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 17),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              productDescription!,
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  color: Colors.white.withOpacity(0.4)),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            SizedBox(
+                              // width: MediaQuery.of(context).size.width / 3.8,
+                              child: Row(
+                                children: [
+                                  // Text(
+                                  //   "6372",
+                                  //   style: TextStyle(
+                                  //       color: Colors.white.withOpacity(0.5),
+                                  //       fontSize: 12),
+                                  // // ),
+                                  // SizedBox(
+                                  //   width: 10,
+                                  // ),
+                                  Text(
+                                    "Date Created: ",
+                                    style: TextStyle(
+                                        fontSize: 12,
+                                        color: Colors.white.withOpacity(0.5),
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    "  " + date!,
+                                    style: TextStyle(
+                                        fontSize: 14,
+                                        color: Colors.white.withOpacity(0.5)),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(
+                              // width: 100,
+                              child: Text(
+                                "Status:",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.white.withOpacity(0.6),
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 8.0),
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    Icons.lock_outline,
+                                    color: _detectAccountColor()!,
+                                  ),
+                                  Text(" - " + acccountStatus!,
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          color: _detectAccountColor()!,
+                                          fontWeight: FontWeight.bold)),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        Image.asset(
+                          productImage!,
+                          height: MediaQuery.of(context).size.width / 10,
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
