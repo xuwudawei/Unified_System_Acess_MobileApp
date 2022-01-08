e177e42e2158dfb611adcfc89a5b274a78c6ddd6
xuwudawei <xuwudawei@outlook.com>
2022-01-08 23:06:38 +0000

    updated provider for product model

--- lib/providers/productListProvider.dart
+++ lib/providers/productListProvider.dart
@@ -3,9 +3,17 @@ import 'package:usa_app/models/product_model.dart';
 
 class ProductListProvider extends ChangeNotifier {
   List<ProductModel> _products = [];
-  List<ProductModel> get getAllProducts => _products;
+  List<ProductModel> getAllProducts() {
+    return _products;
+  }
+
   void setProductList(List<ProductModel> a) {
     _products = a;
+    notifyListeners();
+  }
+
+  void setProductAccountStatusInactive(int index) {
+    _products[index].accountStatus = 0;
     notifyListeners();
   }
 }
