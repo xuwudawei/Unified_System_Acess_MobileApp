652d843508263c7883b2a9dc18cfa26fe5970c5a
xuwudawei <xuwudawei@outlook.com>
2022-01-08 23:03:20 +0000

    updated user mobile screen orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:usa_app/app.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(App());
 }
